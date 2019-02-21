bc9463f05f8de2d8560b4cb0c585a5a0a8220575
fusu <dongxu_zou@126.com>
2019-02-21 16:36:00 +0800

    '样式修改'

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="card-footer">
+    <nav class="navbar">
+        <div>
+            <img width="20" height="20" src="https://lccdn.phphub.org/uploads/images/201808/09/1/fh3NlAlE26.png" alt="#">
+            <a class="navbar-brand" href="https://learnku.com/laravel/courses">刻意练习，每日精进</a>
+        </div>
+
+        <ul class="nav nav-pills">
+            <li><a href="{{ route('about') }}">关于</a></li>
+        </ul>
+    </nav>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="zh-cn">
 <head>
-    <title>@yield('title', 'Weibo App')</title>
+    <title>@yield('title', 'Weibo App') -- Laravel入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include("layouts._header")
+    <div class="container">
+        @yield('content')
+        @include("layouts._footer")
+    </div>
 </body>
 </html>
