496b2b2a42ce1c529ad1572363fbbb89f46c0404
fusu <dongxu_zou@126.com>
2019-02-22 10:59:50 +0800

    'style'

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,12 +1,9 @@
-<footer class="card-footer">
-    <nav class="navbar">
-        <div>
-            <img width="20" height="20" src="https://lccdn.phphub.org/uploads/images/201808/09/1/fh3NlAlE26.png" alt="#">
-            <a class="navbar-brand" href="https://learnku.com/laravel/courses">刻意练习，每日精进</a>
-        </div>
-
-        <ul class="nav nav-pills">
-            <li><a href="{{ route('about') }}">关于</a></li>
-        </ul>
-    </nav>
+<footer class="footer">
+    <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="#">
+    <a href="https://learnku.com/laravel/courses" target="_blank">
+        刻意联系,每日精进
+    </a>
+    <div class="float-right">
+        <a href="{{ route('about') }}">关于</a>
+    </div>
 </footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,8 +7,10 @@
 <body>
     @include("layouts._header")
     <div class="container">
+        <div class="offset-md-1 col-md-10">
         @yield('content')
         @include("layouts._footer")
+        </div>
     </div>
 </body>
 </html>
