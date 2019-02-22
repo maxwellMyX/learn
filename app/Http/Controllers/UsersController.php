910442156a3b3fedc1aeeb16c8fda41832593d87
fusu <dongxu_zou@126.com>
2019-02-22 17:28:08 +0800

    用户注册页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Modules\UsersService;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    private $request;
+
+    private $usersService;
+
+    public function __construct(Request $request, UsersService $usersService)
+    {
+        $this->request = $request;
+        $this->usersService = $usersService;
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+}
---
+++ app/Http/Modules/UsersService.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fusu
+ * Date: 2019/2/22
+ * Time: 16:43
+ */
+
+namespace App\Http\Modules;
+
+
+class UsersService
+{
+    public $aaa;
+    public function __construct()
+    {
+        $this->aaa = 1;
+    }
+
+    public function register()
+    {
+
+    }
+}
